fef73b87e6b6b7a5166e6830d6ed8f061a802e94
giftbatolu <sekinatoyelami@gmail.com>
2026-04-02 10:16:05 +0100

    update some files

--- micro/micro/settings.py
+++ micro/micro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'micro_app',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ALLOWED_HOSTS = ["*"]
