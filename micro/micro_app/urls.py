2d7b0b044e829ca29f2f2cebefae50b447ea5e0c
giftbatolu <sekinatoyelami@gmail.com>
2026-04-02 11:52:48 +0100

    update some files

--- micro/micro_app/urls.py
+++ micro/micro_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import MicroListCreateView
 
 urlpatterns = [
-    path('', MicroListCreateView.as_view(), name='micro'),
+    path('', MicroListCreateView.as_view()),
 ]
