3f30b2ea062857ab6bbd4316b13ce7971998dd1c
giftbatolu <sekinatoyelami@gmail.com>
2026-04-02 11:45:21 +0100

    update some files

--- micro/micro/urls.py
+++ micro/micro/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('micro_app.urls')),
+    path('api/micro/', include('micro_app.urls')),
 ]
